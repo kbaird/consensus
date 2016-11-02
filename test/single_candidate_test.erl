c7c42720dbeae971fd4281f00bcfba149f8a50ea
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-02 15:21:19 -0700

    Add failing multi-voter test

--- test/single_candidate_test.erl
+++ test/single_candidate_test.erl
@@ -12,12 +12,17 @@ single_candidate_test_() ->
     {setup, fun single_candidate_setup/0,
             fun single_candidate_teardown/1,
             [
-                fun single_candidate_single_voter_case/0
+                fun single_candidate_single_voter_case/0,
+                fun single_candidate_multiple_voter_case/0
             ]
     }.
 
 single_candidate_single_voter_case() ->
     Winner = schulze_vote:winner([a]),
+    ?assertEqual(a, Winner).
+
+single_candidate_multiple_voter_case() ->
+    Winner = schulze_vote:winner([a, a, a, a]),
     ?assertEqual(a, Winner).
 
 %%% PRIVATE FUNCTIONS
