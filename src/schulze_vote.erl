c77dab19627867e0c3e8def1874253896dfbeb88
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-02 16:00:03 -0700

    Unordered majority tests now pass with messy code

--- src/schulze_vote.erl
+++ src/schulze_vote.erl
@@ -15,8 +15,17 @@
 make_ballot(Ballot) -> Ballot.
 
 -spec winner([ballot(), ...]) -> candidate().
-winner([Candidate | _]) -> hd(Candidate).
+winner(Ballots) -> winner(Ballots, #{}).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+winner([Ballot | Bs], Acc) ->
+    Candidate = hd(Ballot),
+    Count     = maps:get(Candidate, Acc, 0),
+    winner(Bs, maps:put(Candidate, Count+1, Acc));
+winner([],            Acc) ->
+    L = maps:to_list(Acc),
+    Most = fun({X, Xcnt}, {Y, Ycnt}) -> Xcnt >= Ycnt end,
+    {Candidate, Count} = hd(lists:sort(Most, L)),
+    Candidate.
