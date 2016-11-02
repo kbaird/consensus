ad9795fda135d7a0cb7778704ba73bdb9cdc3aa5
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-02 16:05:48 -0700

    Reorder clauses

--- src/schulze_vote.erl
+++ src/schulze_vote.erl
@@ -20,13 +20,12 @@ winner(Ballots) -> winner(Ballots, #{}).
 %%====================================================================
 %% Internal functions
 %%====================================================================
+winner([], Acc) when is_map(Acc) -> winner([], maps:to_list(Acc));
+winner([], Acc) ->
+    Most = fun({_, Xcnt}, {_, Ycnt}) -> Xcnt >= Ycnt end,
+    {Candidate, _} = hd(lists:sort(Most, Acc)),
+    Candidate;
 winner([Ballot | Bs], Acc) ->
     Candidate = hd(Ballot),
     Count     = maps:get(Candidate, Acc, 0),
-    winner(Bs, maps:put(Candidate, Count+1, Acc));
-winner([],            Acc) when is_map(Acc) ->
-    winner([], maps:to_list(Acc));
-winner([],            Acc) ->
-    Most = fun({_, Xcnt}, {_, Ycnt}) -> Xcnt >= Ycnt end,
-    {Candidate, _} = hd(lists:sort(Most, Acc)),
-    Candidate.
+    winner(Bs, maps:put(Candidate, Count+1, Acc)).
