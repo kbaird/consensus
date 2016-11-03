d1992245c9da182b727a4493d6cf55afd46d3fc7
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-02 19:52:08 -0700

    Rename tests and test files

--- test/one_candidate_test.erl
+++ test/one_candidate_test.erl
@@ -1,26 +1,26 @@
--module('single_candidate_test').
+-module('one_candidate_test').
 -author('Kevin C. Baird').
 
 -include_lib("eunit/include/eunit.hrl").
 
-single_candidate_setup()     -> ok.
-single_candidate_teardown(_) -> ok.
+one_candidate_setup()     -> ok.
+one_candidate_teardown(_) -> ok.
 
-single_candidate_test_() ->
-    {setup, fun single_candidate_setup/0,
-            fun single_candidate_teardown/1,
+one_candidate_test_() ->
+    {setup, fun one_candidate_setup/0,
+            fun one_candidate_teardown/1,
             [
-                fun single_candidate_single_voter_case/0,
-                fun single_candidate_multiple_voter_case/0
+                fun one_candidate_one_voter_case/0,
+                fun one_candidate_multiple_voter_case/0
             ]
     }.
 
-single_candidate_single_voter_case() ->
+one_candidate_one_voter_case() ->
     Ballot = schulze_vote:make_ballot([a]),
     Winner = schulze_vote:winner([Ballot]),
     ?assertEqual(a, Winner).
 
-single_candidate_multiple_voter_case() ->
+one_candidate_multiple_voter_case() ->
     Ballot1 = schulze_vote:make_ballot([a]),
     Ballot2 = schulze_vote:make_ballot([a]),
     Ballot3 = schulze_vote:make_ballot([a]),
