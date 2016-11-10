93d0f49c41767e90d12dcc37360261a7d6a4667d
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-10 09:48:19 -0800

    Add skeleton for make_preferences/1

--- src/schulze_vote.erl
+++ src/schulze_vote.erl
@@ -24,6 +24,27 @@ winner(Ballots) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-spec make_preferences([ballot(), ...]) -> map().
+make_preferences(Ballots) ->
+% Input: d[i,j], the number of voters who prefer candidate i to candidate j.
+% Output: p[i,j], the strength of the strongest path from candidate i to candidate j.
+
+% for i from 1 to C
+%    for j from 1 to C
+%       if (i ≠ j) then
+%          if (d[i,j] > d[j,i]) then
+%             p[i,j] := d[i,j]
+%          else
+%             p[i,j] := 0
+%
+% for i from 1 to C
+%    for j from 1 to C
+%       if (i ≠ j) then
+%          for k from 1 to C
+%             if (i ≠ k and j ≠ k) then
+%                p[j,k] := max ( p[j,k], min ( p[j,i], p[i,k] ) )
+    #{}.
+
 -spec make_candidate(name()) -> candidate().
 make_candidate(Name) -> #candidate{name = Name}.
 
