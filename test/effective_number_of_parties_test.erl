922ec175e2344766ed9aa6b20a0e3512322d2a35
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-15 16:49:15 -0800

    Add near-even 2 party case

--- test/effective_number_of_parties_test.erl
+++ test/effective_number_of_parties_test.erl
@@ -15,6 +15,7 @@ effective_num_parties_test_() ->
                 fun effective_num_parties_dictatorship_case/0,
                 fun effective_num_parties_even_two_case/0,
                 fun effective_num_parties_even_ten_case/0,
+                fun effective_num_parties_near_even_two_case/0,
                 fun effective_num_parties_skewed_two_case/0,
                 fun effective_num_parties_skewed_three_case/0
             ]
@@ -33,6 +34,11 @@ effective_num_parties_even_ten_case() ->
     ?assert(Count > 9.95),
     ?assert(Count < 10.05).
 
+effective_num_parties_near_even_two_case() ->
+    Count = consensus:effective_num_parties(near_even_two_state()),
+    ?assert(Count > 1.97),
+    ?assert(Count < 2.03).
+
 effective_num_parties_skewed_two_case() ->
     Count = consensus:effective_num_parties(skewed_two_state()),
     ?assert(Count > 1.65),
@@ -45,10 +51,11 @@ effective_num_parties_skewed_three_case() ->
 
 %%% PRIVATE FUNCTIONS
 
-single_party_state() -> [ {ccp, 1} ].
-even_two_state()     -> [ {left, 0.5}, {right, 0.5} ].
-even_ten_state()     -> [ {a, 0.1}, {b, 0.1}, {c, 0.1}, {d, 0.1}, {e, 0.1},
-                          {f, 0.1}, {g, 0.1}, {h, 0.1}, {i, 0.1}, {j, 0.1} ].
-skewed_two_state()   -> [ {strong, 0.7}, {weak, 0.3} ].
-skewed_three_state() -> [ {strong, 0.45}, {middle, 0.4}, {weak, 0.15} ].
+single_party_state()  -> [ {ccp, 1} ].
+even_two_state()      -> [ {left, 0.5}, {right, 0.5} ].
+even_ten_state()      -> [ {a, 0.1}, {b, 0.1}, {c, 0.1}, {d, 0.1}, {e, 0.1},
+                           {f, 0.1}, {g, 0.1}, {h, 0.1}, {i, 0.1}, {j, 0.1} ].
+near_even_two_state() -> [ {left, 0.55}, {right, 0.45} ].
+skewed_two_state()    -> [ {strong, 0.7}, {weak, 0.3} ].
+skewed_three_state()  -> [ {strong, 0.45}, {middle, 0.4}, {weak, 0.15} ].
 
