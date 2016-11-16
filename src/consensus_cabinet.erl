5ad76693729ec440ad3541119d21fe59af45a6b1
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-16 15:53:50 -0800

    More visual separation

--- src/consensus_cabinet.erl
+++ src/consensus_cabinet.erl
@@ -14,40 +14,40 @@
 % Cf. Lijphart, Arend, _Patterns of Democracy_, 1999. pg93.
 -spec compose(atom(), [{party_name(), seat_share()}, ...]) ->
     [party_name(), ...].
-compose(bargaining_proposition, SeatShares) ->
-    compose(bp, SeatShares);
+compose(bargaining_proposition, SeatShares) -> compose(bp, SeatShares);
 compose(bp, _SeatShares) ->
     [];
-compose(minimal_connected_winning, SeatShares) ->
-    compose(mcw, SeatShares);
-compose(minimum_connected_winning, SeatShares) ->
-    compose(mcw, SeatShares);
+
+
+compose(minimal_connected_winning, SeatShares) -> compose(mcw, SeatShares);
+compose(minimum_connected_winning, SeatShares) -> compose(mcw, SeatShares);
 compose(mcw, _SeatShares) ->
     [];
-compose(minimal_range, SeatShares) ->
-    compose(mr, SeatShares);
-compose(minimum_range, SeatShares) ->
-    compose(mr, SeatShares);
+
+
+compose(minimal_range, SeatShares) -> compose(mr, SeatShares);
+compose(minimum_range, SeatShares) -> compose(mr, SeatShares);
 compose(mr, _SeatShares) ->
     [];
-compose(minimal_winning_coalition, SeatShares) ->
-    compose(mwc, SeatShares);
-compose(minimum_winning_coalition, SeatShares) ->
-    compose(mwc, SeatShares);
+
+
+compose(minimal_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
+compose(minimum_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
 compose(mwc, _SeatShares) ->
     [[]];
-compose(minimal_size, SeatShares) ->
-    compose(ms, SeatShares);
-compose(minimum_size, SeatShares) ->
-    compose(ms, SeatShares);
+
+
+compose(minimal_size, SeatShares) -> compose(ms, SeatShares);
+compose(minimum_size, SeatShares) -> compose(ms, SeatShares);
 compose(ms, SeatShares) ->
     Cabs    = compose(mwc, SeatShares),
     Sizes   = [ length(C) || C <- Cabs ],
     [ MinSize | _ ] = lists:sort(Sizes),
     [ SmallestCab || SmallestCab <- Cabs,
                      length(SmallestCab) =:= MinSize ];
-compose(policy_viable_coalition, SeatShares) ->
-    compose(pvc, SeatShares);
+
+
+compose(policy_viable_coalition, SeatShares) -> compose(pvc, SeatShares);
 compose(pvc, _SeatShares) ->
     [].
 
