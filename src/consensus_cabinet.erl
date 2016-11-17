8603071dbd5b10a73b2fa901c6252cac6af96c12
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-16 19:28:20 -0800

    permutations -> powerset

--- src/consensus_cabinet.erl
+++ src/consensus_cabinet.erl
@@ -44,10 +44,12 @@ compose(ms, SeatShares) ->
 compose(minimal_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
 compose(minimum_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
 compose(mwc, SeatShares) ->
-    Perms       = permutations(SeatShares),
-    Coalitions  = lists:filter(fun is_coalition/1, Perms),
+    PSet       	= powerset(SeatShares),
+    Unique      = uniqueify(PSet),
+    Coalitions  = lists:filter(fun is_coalition/1, Unique),
     Winners     = lists:filter(fun(C) -> is_winner(C, SeatShares) end, Coalitions),
-    lists:filter(fun(C) -> not too_large(C, Perms) end, Winners).
+    WithSeats   = lists:filter(fun(C) -> not too_large(C, PSet) end, Winners),
+    [ lists:map(fun({Name, _Cnt}) -> Name end, WithSeat) || WithSeat <- WithSeats ];
 
 compose(policy_viable_coalition, SeatShares) -> compose(pvc, SeatShares);
 compose(pvc, _SeatShares) ->
@@ -56,21 +58,32 @@ compose(pvc, _SeatShares) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-is_coalition(Cand) -> length(Cand) > 1.
+is_coalition([{_,_}]) -> false;
+is_coalition(_)       -> true.
 
 is_winner(Coalition, SeatShares) ->
     share(Coalition) > share(SeatShares) / 2.0.
 
-permutations([]) -> [[]];
-permutations(L)  ->
-    [[H|T] || H <- L, T <- permutations(L--[H])].
+powerset([]) -> [[]];
+powerset([H|T]) ->
+    PT = powerset(T),
+    powerset(H, PT, PT).
 
+powerset(_, [],    Acc) -> Acc;
+powerset(X, [H|T], Acc) ->
+    powerset(X, T, [[X|H]|Acc]).
+
+share({_Name, Cnt}) -> Cnt;
 share(L) ->
     lists:foldl(fun({_N, Cnt}, Sum) -> Cnt + Sum end, 0, L).
 
-too_large(C, Perms) ->
-    InC = fun(Party) -> lists:member(Party, C) end,
-    lists:any([ Perm || Perm    <- Perms,
-                        Parties <- Perm,
-                        length(Parties) < length(C),
-                        lists:all(InC, Parties) ]).
+too_large(C, _Perms) ->
+    length(C) > 3.
+    %InC = fun(Party) -> lists:member(Party, C) end,
+    %lists:any([ Perm || Perm    <- Perms,
+    %                    Parties <- Perm,
+    %                    length(Parties) < length(C),
+    %                    lists:all(InC, Parties) ]).
+
+uniqueify(Ls) ->
+    lists:usort([ lists:usort(L) || L <- Ls ]).
