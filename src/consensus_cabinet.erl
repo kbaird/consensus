16f44463c6899d3063d7095a57f744da4879a8d8
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-16 20:42:43 -0800

    ms test passing

--- src/consensus_cabinet.erl
+++ src/consensus_cabinet.erl
@@ -15,8 +15,11 @@
 -spec compose(atom(), [{party_name(), seat_share()}, ...]) ->
     [party_name(), ...].
 compose(bargaining_proposition, SeatShares) -> compose(bp, SeatShares);
-compose(bp, _SeatShares) ->
-    [];
+compose(bp, SeatShares) ->
+    JustParties = compose(mwc, SeatShares),
+    NumParties  = [ length(Ps) || Ps <- JustParties ],
+    Min         = lists:min(NumParties),
+    [ Cab || Cab <- JustParties, length(Cab) =:= Min ];
 
 
 compose(minimal_connected_winning, SeatShares) -> compose(mcw, SeatShares);
@@ -34,22 +37,18 @@ compose(mr, _SeatShares) ->
 compose(minimal_size, SeatShares) -> compose(ms, SeatShares);
 compose(minimum_size, SeatShares) -> compose(ms, SeatShares);
 compose(ms, SeatShares) ->
-    Cabs    = compose(mwc, SeatShares),
-    Sizes   = [ length(C) || C <- Cabs ],
-    [ MinSize | _ ] = lists:sort(Sizes),
-    [ SmallestCab || SmallestCab <- Cabs,
-                     length(SmallestCab) =:= MinSize ];
+    Cabs      = mwc_with_seats(SeatShares),
+    Shares    = [ share(C) || C <- Cabs ],
+    MinSize   = lists:min(Shares),
+    WithSeats = [ Cab || Cab <- Cabs, share(Cab) =:= MinSize ],
+    just_party_names(WithSeats);
 
 
 compose(minimal_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
 compose(minimum_winning_coalition, SeatShares) -> compose(mwc, SeatShares);
 compose(mwc, SeatShares) ->
-    PSet       	= powerset(SeatShares),
-    Unique      = uniqueify(PSet),
-    Coalitions  = lists:filter(fun is_coalition/1, Unique),
-    Winners     = lists:filter(fun(C) -> is_winner(C, SeatShares) end, Coalitions),
-    WithSeats   = lists:filter(fun(C) -> not too_large(C, Winners) end, Winners),
-    [ lists:map(fun({Name, _Cnt}) -> Name end, WithSeat) || WithSeat <- WithSeats ];
+    WithSeats = mwc_with_seats(SeatShares),
+    just_party_names(WithSeats);
 
 compose(policy_viable_coalition, SeatShares) -> compose(pvc, SeatShares);
 compose(pvc, _SeatShares) ->
@@ -63,6 +62,16 @@ is_coalition(_)       -> true.
 
 is_winner(Coalition, SeatShares) ->
     share(Coalition) > share(SeatShares) / 2.0.
+
+just_party_names(Ls) ->
+    [ lists:map(fun({Name, _Cnt}) -> Name end, L) || L <- Ls ].
+
+mwc_with_seats(SeatShares) ->
+    PSet       	= powerset(SeatShares),
+    Unique      = uniqueify(PSet),
+    Coalitions  = lists:filter(fun is_coalition/1, Unique),
+    Winners     = lists:filter(fun(C) -> is_winner(C, SeatShares) end, Coalitions),
+    lists:filter(fun(C) -> not too_large(C, Winners) end, Winners).
 
 powerset([]) -> [[]];
 powerset([H|T]) ->
