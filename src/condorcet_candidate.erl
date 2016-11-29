120a82a93534c6cf06256cc25f07ef22a7c0712f
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-11-28 19:22:14 -0800

    Pull fun construction into private by_least_votes_fun/1

--- src/condorcet_candidate.erl
+++ src/condorcet_candidate.erl
@@ -16,18 +16,19 @@ make(Name) -> #candidate{name = Name}.
 
 -spec rank(list(), map()) -> [candidate(), ...].
 rank(Candidates, Prefs) ->
-    % I think this may actually be closer to
-    % https://en.wikipedia.org/wiki/Ranked_pairs than Schulze
-    % (both satisfy Condorcet criteria)
-    ByLeastVotes = fun(C1, C2) ->
-        maps:get(C1, maps:get(C2, Prefs), 0) <
-        maps:get(C2, maps:get(C1, Prefs), 0)
-    end,
     % sort by least votes, so the lowest magnitude for "least votes"
     % (i.e., the winner, with the highest number of votes) is at the front.
+    ByLeastVotes = by_least_votes_fun(Prefs),
     lists:sort(ByLeastVotes, Candidates).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-
+by_least_votes_fun(Prefs) ->
+    % I think this may actually be closer to
+    % https://en.wikipedia.org/wiki/Ranked_pairs than Schulze
+    % (both satisfy Condorcet criteria)
+    fun(C1, C2) ->
+        maps:get(C1, maps:get(C2, Prefs), 0) <
+        maps:get(C2, maps:get(C1, Prefs), 0)
+    end.
