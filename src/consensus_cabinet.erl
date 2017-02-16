6c718352d0eb6e8660aeb0c8cc9ab9849772cb2c
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-02-16 15:21:32 -0800

    Add specs to consensus_cabinet private functions

--- src/consensus_cabinet.erl
+++ src/consensus_cabinet.erl
@@ -67,7 +67,7 @@ centrist_party(Parties) ->
     centrist_party(Sub).
 
 % Does this coalition avoid gaps between parties?
--spec contiguous([cabinet()]) -> boolean().
+-spec contiguous(cabinet()) -> boolean().
 contiguous(Cabinet) ->
     PartyNames  = party_names(Cabinet),
     PartyVals   = lists:map(fun atom_to_ascii/1, PartyNames),
@@ -77,13 +77,16 @@ contiguous(Cabinet) ->
     AllParties  = lists:seq(atom_to_ascii(Lo), atom_to_ascii(Hi)),
     all_in(PartyVals, AllParties).
 
+-spec is_coalition(cabinet()) -> boolean().
 is_coalition([ _ ]) -> false; % has only 1 party in the list
 is_coalition(_)     -> true.
 
 % Does this coalition command a majority of seats?
+-spec is_winner([cabinet()], [cabinet()]) -> boolean().
 is_winner(Coalition, SeatShares) ->
     share(Coalition) > share(SeatShares) / 2.0.
 
+-spec just_party_names([cabinet()]) -> [[atom()]].
 just_party_names(Cabs) ->
     [ party_names(Cab) || Cab <- Cabs ].
 
@@ -127,6 +130,7 @@ share(L) ->
     lists:foldl(SumShares, 0, L).
 
 % Are any elements in arg2 subsets of arg1?
+-spec too_large(cabinet(), [cabinet()]) -> boolean().
 too_large(Cabinet, Coalitions) ->
     Smallers = [ Coalition ||  Coalition  <- Coalitions,
                     length(Coalition) < length(Cabinet),
@@ -136,6 +140,7 @@ too_large(Cabinet, Coalitions) ->
 uniqueify(Ls) ->
     lists:usort([ lists:usort(L) || L <- Ls ]).
 
+-spec winning_coalitions([cabinet()]) -> [cabinet()].
 winning_coalitions(SeatShares) ->
     PSet        = powerset(SeatShares),
     Unique      = uniqueify(PSet),
