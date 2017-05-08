cd715831b4d7e35b9a5b9d4feef3f6823d5ae457
Kevin C. Baird <kcbaird@world.oberlin.edu>
2017-05-08 12:36:27 -0700

    Add 4-candidate test (ex2)

--- test/schulze_test.erl
+++ test/schulze_test.erl
@@ -7,15 +7,17 @@
 % Schulze
 % Taken from http://wiki.electorama.com/wiki/Schulze_method
 
-schulze_ex1_setup()     -> ok.
-schulze_ex1_teardown(_) -> ok.
+schulze_setup()     -> ok.
+schulze_teardown(_) -> ok.
 
-schulze_ex1_test_() ->
-    {setup, fun schulze_ex1_setup/0,
-            fun schulze_ex1_teardown/1,
+schulze_test_() ->
+    {setup, fun schulze_setup/0,
+            fun schulze_teardown/1,
             [
                 fun schulze_ex1_winner_case/0,
-                fun schulze_ex1_rankings_case/0
+                fun schulze_ex1_rankings_case/0,
+                fun schulze_ex2_winner_case/0,
+                fun schulze_ex2_rankings_case/0
             ]
     }.
 
@@ -26,6 +28,14 @@ schulze_ex1_winner_case() ->
 schulze_ex1_rankings_case() ->
     Rankings = consensus:condorcet_rankings(schulze_ex1_ballots()),
     ?assertEqual([e, a, c, b, d], Rankings).
+
+schulze_ex2_winner_case() ->
+    Winner = consensus:condorcet_winner(schulze_ex2_ballots()),
+    ?assertEqual(d, Winner).
+
+schulze_ex2_rankings_case() ->
+    Rankings = consensus:condorcet_rankings(schulze_ex2_ballots()),
+    ?assertEqual([d, a, c, b], Rankings).
 
 %%% PRIVATE FUNCTIONS
 
@@ -39,6 +49,19 @@ schulze_ex1_ballots() ->
         lists:duplicate(2, ballot([c, b, a, d, e])),
         lists:duplicate(7, ballot([d, c, e, b, a])),
         lists:duplicate(8, ballot([e, b, a, d, c]))
+    ]).
+
+schulze_ex2_ballots() ->
+    lists:flatten([
+        lists:duplicate(5, ballot([a, c, b, d])),
+        lists:duplicate(2, ballot([a, c, d, b])),
+        lists:duplicate(3, ballot([a, d, c, b])),
+        lists:duplicate(4, ballot([b, a, c, d])),
+        lists:duplicate(3, ballot([c, b, d, a])),
+        lists:duplicate(3, ballot([c, d, b, a])),
+        lists:duplicate(1, ballot([d, a, c, b])),
+        lists:duplicate(5, ballot([d, b, a, c])),
+        lists:duplicate(4, ballot([d, c, b, a]))
     ]).
 
 ballot(L) -> condorcet_ballot:make(L).
