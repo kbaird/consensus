3876a6f6450e926627ce2ef0c8941655e8398305
Kevin Baird <kevincbaird@gmail.com>
2017-09-21 14:53:11 -0700

    Add extreme_skew_case() to Gallagher tests

--- test/gallagher_index_test.erl
+++ test/gallagher_index_test.erl
@@ -16,7 +16,8 @@ gallagher_index_test_() ->
                 fun gallagher_index_uk2015parl_case/0,
                 fun gallagher_index_us2016potus_case/0,
                 fun gallagher_index_even_three_case/0,
-                fun gallagher_index_even_two_case/0
+                fun gallagher_index_even_two_case/0,
+                fun gallagher_index_extreme_skew_case/0
             ]
     }.
 
@@ -40,6 +41,11 @@ gallagher_index_even_two_case() ->
     ?assert(GIdx > 49.49),
     ?assert(GIdx < 49.51).
 
+gallagher_index_extreme_skew_case() ->
+    GIdx = consensus:gallagher_index(extreme_skew()),
+    ?assert(GIdx > 63.93),
+    ?assert(GIdx < 63.97).
+
 %%% PRIVATE FUNCTIONS
 
 even(3) -> [
@@ -51,6 +57,12 @@ even(2) -> [
     consensus_party:make(jack_johnson, 1, 50.00),
     consensus_party:make(john_jackson, 1, 50.00)
 ].
+
+extreme_skew() -> [
+    consensus_party:make(usurper, 1, 10.00),
+    consensus_party:make(deposed, 0, 90.00)
+].
+
 uk2015parl()  -> [
     % party_name, seat_share, vote_pc
     consensus_party:make(con, 330, 36.9),
@@ -60,6 +72,7 @@ uk2015parl()  -> [
     consensus_party:make(ukip,  1, 12.3),
     consensus_party:make(grn,   1,  3.8)
 ].
+
 us2016potus() -> [
     % party_name, electoral_votes, vote_pc
     consensus_party:make(gop, 306, 46.72),
