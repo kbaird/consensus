bb25e5312391b5fe5cb13336041809be605a56ae
Kevin Baird <kevincbaird@gmail.com>
2017-09-27 18:04:51 -0700

    Re-order public API funs

--- src/consensus.erl
+++ src/consensus.erl
@@ -3,10 +3,10 @@
 %% API exports
 -export([
     cabinet_composition/2,
-    gallagher_index/1,
-    effective_num_parties/1,
     condorcet_rankings/1,
-    condorcet_winner/1
+    condorcet_winner/1,
+    effective_num_parties/1,
+    gallagher_index/1
 ]).
 
 -include("parties.hrl").
@@ -20,9 +20,14 @@
 cabinet_composition(Label, SeatShares) ->
     consensus_cabinet:compose(Label, SeatShares).
 
-% Implement Markku Laakso and Rein Taagepera's index as described in
-% Arend Lijphart's Patterns of Democracy (1999), pp67-68.
+condorcet_rankings(Ballots) ->
+    consensus_condorcet:rankings(Ballots).
 
+condorcet_winner(Ballots) ->
+    consensus_condorcet:winner(Ballots).
+
+% Implement Markku Laakso and Rein Taagepera's index as described in
+% Arend Lijphart's Patterns of Democracy (1999), pp67-68. also
 % Laakso, Markku and Rein Taagepera. 1979.
 % "'Effective' Number of Parties: A Measure with Application to West Europe."
 % _Comparative Political Studies_ 12, no. 1 (April): 3-27.
@@ -37,12 +42,6 @@ effective_num_parties(PartyShares) -> 1 / sum_for(PartyShares).
 -spec gallagher_index([party_result()]) -> number().
 gallagher_index(ElectionResults) ->
     consensus_index:gallagher(ElectionResults).
-
-condorcet_rankings(Ballots) ->
-    consensus_condorcet:rankings(Ballots).
-
-condorcet_winner(Ballots) ->
-    consensus_condorcet:winner(Ballots).
 
 %%====================================================================
 %% Internal functions
