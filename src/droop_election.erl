f47a4b5185dc0d8eaa8f06c8d618d06bc52645e9
Kevin Baird <kevincbaird@gmail.com>
2018-03-02 10:08:03 -0800

    Add Ballots2 and TODO notes

--- src/droop_election.erl
+++ src/droop_election.erl
@@ -24,19 +24,24 @@ droop_winners(SeatsCount, Ballots, Winners) ->
     Quota = length(Ballots) div (SeatsCount+1) + 1,
     NestedCands = [ ballot:candidates(B) || B <- Ballots ],
     CNamesWithVotes = candidate_names_with_votes(NestedCands),
+    % TODO: What if top vote getter is below quota?
     [ Winner | _ ] = lists:sort(fun({_, V1}, {_, V2}) -> V1 > V2 end, CNamesWithVotes),
-    droop_winners(SeatsCount, ballots_without(Ballots, Winner, Quota, CNamesWithVotes), [ Winner | Winners ]).
+    Ballots2 = ballots_without(Ballots, Winner, Quota, CNamesWithVotes),
+    droop_winners(SeatsCount, Ballots2, [ Winner | Winners ]).
 
 ballots_without(Ballots, {WinnerName, WinnerVotes}, Quota, [ _, _NextCandidate | _ ]) ->
     Raw   = ballots_without(Ballots, {WinnerName, WinnerVotes}, Quota, []),
+    % TODO: stop peeking inside ballot and candidate. Use provided funs.
     Names = [ NextName || {ballot, [{candidate, FirstName}, {candidate, NextName} | _]} <- Ballots,
                           FirstName =:= WinnerName ],
     VoteCountToTransfer = WinnerVotes - Quota,
     Raw ++ [ ballot:make([NextName]) || NextName <- lists:sublist(Names, VoteCountToTransfer) ];
 ballots_without(Ballots, {WinnerName, _WinnerVotes}, _Quota, _) ->
+    % TODO: stop peeking inside ballot and candidate. Use provided funs.
     [ B || B <- Ballots, not lists:member({candidate, WinnerName}, ballot:candidates(B)) ].
 
 candidate_names_with_votes(NestedCs) ->
     CNames = [ candidate:name(C) || C <- lists:usort(lists:flatten(NestedCs)) ],
+    % TODO: stop peeking inside ballot and candidate. Use provided funs.
     [ {N, length(lists:filter(fun([{candidate, Name} | _]) -> Name =:= N end, NestedCs))} || N <- CNames ].
 
