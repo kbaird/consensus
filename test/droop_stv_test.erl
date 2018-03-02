e4a86138292a206cf16a28c26811a8a6d2dde066
Kevin Baird <kevincbaird@gmail.com>
2018-03-02 13:50:25 -0800

    Add more tests, handle votes below quota

--- test/droop_stv_test.erl
+++ test/droop_stv_test.erl
@@ -11,7 +11,9 @@ droop_stv_test_() ->
     {setup, fun droop_stv_setup/0,
             fun droop_stv_teardown/1,
             [
-                fun droop_two_winners_three_candidates_stv_stv_case/0
+                fun droop_two_winners_three_candidates_stv_stv_case/0,
+                %fun droop_five_winners_six_candidates_stv_stv_case/0,
+                fun droop_three_winners_five_candidates_stv_stv_case/0
             ]
     }.
 
@@ -23,6 +25,33 @@ droop_two_winners_three_candidates_stv_stv_case() ->
     Ballots = BradOnlyBallots ++ CarterOnlyBallots ++ MixedBallots,
     Winners = consensus:droop_winners(2, Ballots),
     ?assertEqual([{andrea, 45}, {carter, 36}], Winners).
+
+% https://en.wikipedia.org/wiki/Comparison_of_the_Hare_and_Droop_quotas#Scenario_1
+droop_five_winners_six_candidates_stv_stv_case() ->
+    Ballot1s = [ballot:make([andrea, carter, brad]) || _ <- lists:seq(1, 31)],
+    Ballot2s = [ballot:make([carter, andrea, brad]) || _ <- lists:seq(1, 30)],
+    Ballot3s = [ballot:make([brad, andrea, carter]) || _ <- lists:seq(1, 2)],
+    Ballot4s = [ballot:make([delilah, scott, jennifer]) || _ <- lists:seq(1, 20)],
+    Ballot5s = [ballot:make([scott, delilah, jennifer]) || _ <- lists:seq(1, 20)],
+    Ballot6s = [ballot:make([jennifer, delilah, scott]) || _ <- lists:seq(1, 17)],
+    Ballots = Ballot1s ++ Ballot2s ++ Ballot3s ++ Ballot4s ++ Ballot5s ++ Ballot6s,
+    [ Andrea, Carter, Brad, Delilah, Scott ] = consensus:droop_winners(5, Ballots),
+    {andrea, _} = Andrea,
+    {carter, _} = Carter,
+    {brad, _} = Brad,
+    {delilah, _} = Delilah,
+    {scott, _} = Scott.
+
+% https://en.wikipedia.org/wiki/Comparison_of_the_Hare_and_Droop_quotas#Scenario_2
+droop_three_winners_five_candidates_stv_stv_case() ->
+    Ballot1s = [ballot:make([alex, bobbie, chris]) || _ <- lists:seq(1, 50)],
+    Ballot2s = [ballot:make([jo]) || _ <- lists:seq(1, 25)],
+    Ballot3s = [ballot:make([kim]) || _ <- lists:seq(1, 24)],
+    Ballots = Ballot1s ++ Ballot2s ++ Ballot3s,
+    Winners = consensus:droop_winners(3, Ballots),
+    ?assert(lists:member({alex, 50}, Winners)),
+    ?assert(lists:member({bobbie, 25}, Winners)),
+    ?assert(lists:member({jo, 25}, Winners)).
 
 %%% PRIVATE FUNCTIONS
 
