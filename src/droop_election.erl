df0b35c465a4bc101e1816eaf5841fc7cd4b5bb7
Kevin Baird <kevincbaird@gmail.com>
2018-03-02 09:44:10 -0800

    Rename fun. We didn't care that they were alphabetically sorted, just used usort to make them unique.

--- src/droop_election.erl
+++ src/droop_election.erl
@@ -23,7 +23,7 @@ droop_winners(SeatsCount, _Bs, Winners) when length(Winners) =:= SeatsCount ->
 droop_winners(SeatsCount, Ballots, Winners) ->
     Quota = length(Ballots) div (SeatsCount+1) + 1,
     NestedCands = [ ballot:candidates(B) || B <- Ballots ],
-    CNamesWithVotes = sorted_candidate_names_with_votes(NestedCands),
+    CNamesWithVotes = candidate_names_with_votes(NestedCands),
     [ Winner | _ ] = lists:sort(fun({_, V1}, {_, V2}) -> V1 > V2 end, CNamesWithVotes),
     droop_winners(SeatsCount, ballots_without(Ballots, Winner, Quota, CNamesWithVotes), [ Winner | Winners ]).
 
@@ -36,7 +36,7 @@ ballots_without(Ballots, {WinnerName, WinnerVotes}, Quota, [ _, _NextCandidate |
 ballots_without(Ballots, {WinnerName, _WinnerVotes}, _Quota, _) ->
     [ B || B <- Ballots, not lists:member({candidate, WinnerName}, ballot:candidates(B)) ].
 
-sorted_candidate_names_with_votes(NestedCs) ->
+candidate_names_with_votes(NestedCs) ->
     CNames = [ candidate:name(C) || C <- lists:usort(lists:flatten(NestedCs)) ],
     [ {N, length(lists:filter(fun([{candidate, Name} | _]) -> Name =:= N end, NestedCs))} || N <- CNames ].
 
