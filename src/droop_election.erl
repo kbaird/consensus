7e8e076c65cba78dd984d93775497d1b080cdfd4
Kevin Baird <kevincbaird@gmail.com>
2018-03-01 16:37:46 -0800

    Successfully transfer votes above quota to next choice

--- src/droop_election.erl
+++ src/droop_election.erl
@@ -11,7 +11,7 @@
 %% API functions
 %%====================================================================
 
--spec winners(pos_integer(), [ballot(), ...]) -> [name(), ...].
+-spec winners(pos_integer(), [ballot(), ...]) -> [{name(), pos_integer()}, ...].
 winners(SeatsCount, Ballots) -> droop_winners(SeatsCount, Ballots, []).
 
 %%====================================================================
@@ -21,19 +21,20 @@ winners(SeatsCount, Ballots) -> droop_winners(SeatsCount, Ballots, []).
 droop_winners(SeatsCount, _Bs, Winners) when length(Winners) =:= SeatsCount ->
     lists:reverse(Winners);
 droop_winners(SeatsCount, Ballots, Winners) ->
-    %Quota = length(Ballots) / (SeatsCount+1) + 1,
+    Quota = length(Ballots) div (SeatsCount+1) + 1,
     NestedCands = [ condorcet_ballot:candidates(B) || B <- Ballots ],
     CNamesWithVotes = sorted_candidate_names_with_votes(NestedCands),
     [ Winner | _ ] = lists:sort(fun({_, V1}, {_, V2}) -> V1 > V2 end, CNamesWithVotes),
-    % TODO: Don't give 2nd place candidate all of winner's votes in next round, just the surplus beyond the quota
-    droop_winners(SeatsCount, ballots_without(Ballots, Winner), [ Winner | Winners ]).
+    droop_winners(SeatsCount, ballots_without(Ballots, Winner, Quota, CNamesWithVotes), [ Winner | Winners ]).
 
-ballots_without(Ballots, {WinnerName, _WinnerVotes}) ->
-    CandsWithoutWinner  = fun({candidate, CN}) -> CN =/= WinnerName end,
-    BallotWithoutWinner = fun(B) -> {ballot, lists:filter(CandsWithoutWinner,
-                                                          condorcet_ballot:candidates(B))}
-                          end,
-    lists:map(BallotWithoutWinner, Ballots).
+ballots_without(Ballots, {WinnerName, WinnerVotes}, Quota, [ _, _NextCandidate | _ ]) ->
+    Raw   = ballots_without(Ballots, {WinnerName, WinnerVotes}, Quota, []),
+    Names = [ NextName || {ballot, [{candidate, FirstName}, {candidate, NextName} | _]} <- Ballots,
+                          FirstName =:= WinnerName ],
+    VoteCountToTransfer = WinnerVotes - Quota,
+    Raw ++ [ condorcet_ballot:make([NextName]) || NextName <- lists:sublist(Names, VoteCountToTransfer) ];
+ballots_without(Ballots, {WinnerName, _WinnerVotes}, _Quota, _) ->
+    [ B || B <- Ballots, not lists:member({candidate, WinnerName}, condorcet_ballot:candidates(B)) ].
 
 sorted_candidate_names_with_votes(NestedCands) ->
     CNames = [ condorcet_candidate:name(C) ||
