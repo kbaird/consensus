dd97d36d38478e9675a499382a664adca0b2d9a5
Kevin Baird <kevincbaird@gmail.com>
2018-07-31 06:35:12 -0700

    Add implementation and make test more precise

--- src/party_list_election.erl
+++ src/party_list_election.erl
@@ -19,8 +19,7 @@
 jefferson_dhondt_rankings(Votes, NumberOfSeats) ->
     %%% TODO: real implementation as per
     % https://en.wikipedia.org/wiki/D%27Hondt_method#Allocation
-    rankings(jefferson_dhondt, Votes, NumberOfSeats),
-    [ {a, 4, 3.4}, {b, 3, 2.8}, {c, 1, 1.1}, {d, 0, 0.7} ].
+    rankings(jefferson_dhondt, Votes, NumberOfSeats).
 
 % Returns {PartyName, ActualSeatsWon, ProportionalSeats}
 -spec webster_sainte_lague_rankings([{party_name(), pos_integer()}, ...], pos_integer()) ->
@@ -28,16 +27,20 @@ jefferson_dhondt_rankings(Votes, NumberOfSeats) ->
 webster_sainte_lague_rankings(Votes, NumberOfSeats) ->
     %%% TODO: real implementation as per
     % https://en.wikipedia.org/wiki/Webster/Sainte-Lagu%C3%AB_method#Description_of_the_method
-    rankings(webster_sainte_lague, Votes, NumberOfSeats),
-    [ {a, 3, 3.71}, {b, 2, 1.68}, {c, 2, 1.61} ].
+    rankings(webster_sainte_lague, Votes, NumberOfSeats).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-quotient(jefferson_dhondt, {_PartyName, SeatsSoFar, VoteCount}) ->
+by_highest_quotient({_, _, _, Q1}, {_, _, _, Q2}) -> Q1 > Q2.
+
+by_seats_won({_, S1, _}, {_, S2, _}) -> S1 > S2.
+
+quotient(jefferson_dhondt, SeatsSoFar, VoteCount) ->
     Denominator = SeatsSoFar + 1,
     VoteCount / Denominator;
-quotient(webster_sainte_lague, {_PartyName, SeatsSoFar, VoteCount}) ->
+
+quotient(webster_sainte_lague, SeatsSoFar, VoteCount) ->
     Denominator = SeatsSoFar*2 + 1,
     VoteCount / Denominator.
 
@@ -45,6 +48,20 @@ rankings(Label, Votes, NumberOfSeatsToFill) ->
     Counters = [ {PartyName, 0, VoteCount} || {PartyName, VoteCount} <- Votes ],
     tabulate(Label, Counters, NumberOfSeatsToFill, 0).
 
-tabulate(_, Counters, SeatsFilled, SeatsFilled) -> Counters;
-tabulate(Label, Counters, _SeatsToFill, _SeatsFilled) ->
-    [ quotient(Label, C) || C <- Counters ].
+share(Votes, TotalVotes, SeatsFilled) ->
+    Prec = math:pow(10, 2),
+    trunc(Votes /TotalVotes * SeatsFilled * Prec) / Prec.
+
+tabulate(_, Counters, SeatsFilled, SeatsFilled) ->
+    VoteCounts = [ VoteCount || {_PN, _SSF, VoteCount} <- Counters ],
+    TotalVotes = lists:sum(VoteCounts),
+    Sorted     = lists:sort(fun by_seats_won/2, Counters),
+    [ {PN, PartySeats, share(VoteCount, TotalVotes, SeatsFilled)} || {PN, PartySeats, VoteCount} <- Sorted ];
+
+tabulate(Label, Counters, SeatsToFill, SeatsFilled) ->
+    Counters2 = [ {PartyName, SeatsSoFar, VoteCount, quotient(Label, SeatsSoFar, VoteCount)} ||
+                  {PartyName, SeatsSoFar, VoteCount} <- Counters ],
+    [ {WinPN, WinSeatsSoFar, WinVC, WinQ} | Rest ] = lists:sort(fun by_highest_quotient/2, Counters2),
+    Counters3 = [ {WinPN, WinSeatsSoFar+1, WinVC, WinQ} | Rest ],
+    Counters4 = [ {PartyName, SeatsSoFar, VoteCount} || {PartyName, SeatsSoFar, VoteCount, _Q} <- Counters3 ],
+    tabulate(Label, Counters4, SeatsToFill, SeatsFilled+1).
--- test/jefferson_dhondt_test.erl
+++ test/jefferson_dhondt_test.erl
@@ -19,7 +19,7 @@ jefferson_dhondt_test_() ->
 jefferson_dhondt_basic_case() ->
     Votes    = [ {a, 100000}, {b, 80000}, {c, 30000}, {d, 20000} ],
     Rankings = consensus:jefferson_dhondt_rankings(Votes, 8),
-    ?assertEqual([{a, 4, 3.4}, {b, 3, 2.8}, {c, 1, 1.1}, {d, 0, 0.7}], Rankings).
+    ?assertEqual([{a, 4, 3.47}, {b, 3, 2.78}, {c, 1, 1.04}, {d, 0, 0.69}], Rankings).
 
 %%% PRIVATE FUNCTIONS
 
