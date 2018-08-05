35bf2ece167057d6a34b0bd23fad3b0100a18a7a
Kevin Baird <kevincbaird@gmail.com>
2018-08-04 17:53:25 -0700

    More specs, fewer guards

--- src/borda.erl
+++ src/borda.erl
@@ -19,11 +19,11 @@ rankings(Label, Ballots) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-add_votes(Label, [C | Cs], Acc) when is_map(Acc) ->
-    add_votes(Label, [C | Cs], Acc, 1).
+-spec add_votes(atom(), [candidate(), ...], map()) -> map().
+add_votes(Label, [C | Cs], Acc) when is_map(Acc) -> add_votes(Label, [C | Cs], Acc, 1).
 
-add_votes(_,     [],       Acc, _)        when is_map(Acc) -> Acc;
-add_votes(Label, [C | Cs], Acc, Position) when is_map(Acc) ->
+add_votes(_,     [],       Acc, _) -> Acc;
+add_votes(Label, [C | Cs], Acc, Position) ->
     CN = candidate:name(C),
     VotesByPosition = maps:get(CN, Acc, #{}),
     VotesAtPosition = maps:get(Position, VotesByPosition, 0),
@@ -31,11 +31,12 @@ add_votes(Label, [C | Cs], Acc, Position) when is_map(Acc) ->
     Acc2 = maps:put(CN, NewVotes, Acc),
     add_votes(Label, Cs, Acc2, Position + 1).
 
+-spec borda_values(atom(), pos_integer(), map()) -> map().
 borda_values(Label, CandCount, Acc) when is_atom(Label) andalso is_map(Acc) ->
     borda_values(Label, CandCount, maps:keys(Acc), Acc, #{}).
 
 borda_values(_label, _CandCount, [], #{}, Final) -> Final;
-borda_values(Label, CandCount, [CandName | CNs], From, Acc) when is_map(From) andalso is_map(Acc) ->
+borda_values(Label, CandCount, [CandName | CNs], From, Acc) ->
     Votes = maps:get(CandName, From),
     From2 = maps:without([CandName], From),
     Acc2 = maps:put(CandName, value_of_votes(Label, CandCount, Votes), Acc),
@@ -47,6 +48,7 @@ rankings(Label, [B | Bs], CandCount, Acc) when is_map(Acc) ->
     Acc2 = add_votes(Label, Candidates, Acc),
     rankings(Label, Bs, CandCount, Acc2).
 
+-spec value(atom(), pos_integer(), pos_integer()) -> non_neg_integer().
 value(base0, Position, CandCount) -> CandCount - Position;
 
 value(base1, Position, CandCount) -> value(base0, Position, CandCount) + 1;
@@ -54,6 +56,7 @@ value(base1, Position, CandCount) -> value(base0, Position, CandCount) + 1;
 value(naura, 1,        _CandCount) -> 1;
 value(naura, Position, _CandCount) -> 1.0 / Position.
 
+-spec value_of_votes(atom(), pos_integer(), map()) -> non_neg_integer().
 value_of_votes(Label, CandCount, Votes) when is_map(Votes) ->
     Values = lists:map(fun(Idx) ->
                             EachValue = value(Label, Idx, CandCount),
