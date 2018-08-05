5198ddd38ed5e247fd1f3156315054883c32702e
Kevin Baird <kevincbaird@gmail.com>
2018-08-04 17:26:39 -0700

    Rename CandidateCount -> CandCount

--- src/borda.erl
+++ src/borda.erl
@@ -31,31 +31,31 @@ add_votes(Label, [C | Cs], Acc, Position) when is_map(Acc) ->
     Acc2 = maps:put(CN, NewVotes, Acc),
     add_votes(Label, Cs, Acc2, Position + 1).
 
-convert_to_borda_values(Label, CandidateCount, Acc) when is_atom(Label) andalso is_map(Acc) ->
+convert_to_borda_values(Label, CandCount, Acc) when is_atom(Label) andalso is_map(Acc) ->
     CandidateNames = maps:keys(Acc),
-    convert_to_borda_values(Label, CandidateCount, CandidateNames, Acc, #{}).
+    convert_to_borda_values(Label, CandCount, CandidateNames, Acc, #{}).
 
-convert_to_borda_values(_,     _,              [],         #{},  Final) -> Final;
-convert_to_borda_values(Label, CandidateCount, [CN | CNs], From, Acc) when is_map(From) andalso is_map(Acc) ->
+convert_to_borda_values(_label, _CandCount, [], #{}, Final) -> Final;
+convert_to_borda_values(Label, CandCount, [CN | CNs], From, Acc) when is_map(From) andalso is_map(Acc) ->
     Votes = maps:get(CN, From),
     From2 = maps:without([CN], From),
-    Acc2 = maps:put(CN, tally_votes(Label, CandidateCount, Votes), Acc),
-    convert_to_borda_values(Label, CandidateCount, CNs, From2, Acc2).
+    Acc2 = maps:put(CN, tally_votes(Label, CandCount, Votes), Acc),
+    convert_to_borda_values(Label, CandCount, CNs, From2, Acc2).
 
-rankings(Label, [],       CandidateCount, Acc) -> convert_to_borda_values(Label, CandidateCount, Acc);
-rankings(Label, [B | Bs], CandidateCount, Acc) when is_map(Acc) ->
+rankings(Label, [],       CandCount, Acc) -> convert_to_borda_values(Label, CandCount, Acc);
+rankings(Label, [B | Bs], CandCount, Acc) when is_map(Acc) ->
     Candidates = ballot:candidates(B),
     Acc2 = add_votes(Label, Candidates, Acc),
-    rankings(Label, Bs, CandidateCount, Acc2).
+    rankings(Label, Bs, CandCount, Acc2).
 
 tally_votes(Label, CandCount, Votes) when is_map(Votes) ->
     Values = [ value(Label, Idx, CandCount) * maps:get(Idx, Votes) || Idx <- maps:keys(Votes) ],
     lists:sum(Values).
 
 value(base0, Last, Last) -> 0;
-value(base0, Position, CandidateCount) -> value(base0, Position + 1, CandidateCount) + 1;
+value(base0, Position, CandCount) -> value(base0, Position + 1, CandCount) + 1;
 
-value(base1, Position, CandidateCount) -> value(base0, Position, CandidateCount) + 1;
+value(base1, Position, CandCount) -> value(base0, Position, CandCount) + 1;
 
-value(naura, 1,        _CandidateCount) -> 1;
-value(naura, Position, _CandidateCount) -> 1.0 / Position.
+value(naura, 1,        _CandCount) -> 1;
+value(naura, Position, _CandCount) -> 1.0 / Position.
