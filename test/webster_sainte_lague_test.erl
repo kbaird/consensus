4b86b5a1d23f61264fc7d48d5dfb8116a7a631fc
Kevin Baird <kevincbaird@gmail.com>
2019-04-01 10:52:41 -0700

    Add 2016 Rhineland-Palatinate state election test for threshold

--- test/webster_sainte_lague_test.erl
+++ test/webster_sainte_lague_test.erl
@@ -12,7 +12,8 @@ webster_sainte_lague_test_() ->
     {setup, fun webster_sainte_lague_setup/0,
             fun webster_sainte_lague_teardown/1,
             [
-                fun webster_sainte_lague_basic_case/0
+                fun webster_sainte_lague_basic_case/0,
+                fun webster_sainte_lague_2016_rhineland_palatinate_case/0
             ]
     }.
 
@@ -20,6 +21,12 @@ webster_sainte_lague_basic_case() ->
     Votes    = [ {a, 53000}, {b, 24000}, {c, 23000} ],
     Rankings = consensus:webster_sainte_lague_rankings(Votes, 7),
     ?assertEqual([{a, 3, 3.71}, {b, 2, 1.68}, {c, 2, 1.61}], Rankings).
+
+% https://en.wikipedia.org/wiki/2016_Rhineland-Palatinate_state_election
+webster_sainte_lague_2016_rhineland_palatinate_case() ->
+    Votes    = [ {spd, 771848}, {cdu, 677507}, {afd, 268628}, {fdp, 132294}, {grn, 113261}, {lnk, 60074} ],
+    Rankings = consensus:webster_sainte_lague_rankings(Votes, 101),
+    ?assertEqual([{spd, 39, 36.2}, {cdu, 35, 31.8}, {afd, 14, 12.6}, {fdp, 7, 6.2}, {grn, 6, 5.3}], Rankings).
 
 %%% PRIVATE FUNCTIONS
 
