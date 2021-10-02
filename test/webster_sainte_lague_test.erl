9130301e9805c3fe80bb88a3f5075fb50f58c355
Kevin Baird <kevincbaird@gmail.com>
2021-10-02 11:09:40 -0700

    Add 2021 Federal Bundestag Election

--- test/webster_sainte_lague_test.erl
+++ test/webster_sainte_lague_test.erl
@@ -14,7 +14,8 @@ webster_sainte_lague_test_() ->
                 fun webster_sainte_lague_basic_case/0,
                 fun webster_sainte_lague_2016_baden_wuerttemberg_case/0,
                 fun webster_sainte_lague_2016_rhineland_palatinate_case/0,
-                fun webster_sainte_lague_2016_saxony_anhalt_case/0
+                fun webster_sainte_lague_2016_saxony_anhalt_case/0,
+                fun webster_sainte_lague_2021_federal_case/0
             ]
     }.
 
@@ -43,4 +44,18 @@ webster_sainte_lague_2016_saxony_anhalt_case() ->
     Votes    = [ {cdu, 334139}, {afd, 272496}, {lnk, 183290}, {spd, 119368}, {grn, 58209}, {fdp, 54565}, {fw, 24269}, {npd, 21230}, {heap, 16611}, {apa, 11653} ],
     Rankings = consensus:webster_sainte_lague_rankings(Votes, 87, 0.05),
     [ {cdu, 30, _}, {afd, 24, _}, {lnk, 16, _}, {spd, 11, _}, {grn, 5, _}] = Rankings.
+
+% https://en.wikipedia.org/wiki/2021_German_federal_election
+webster_sainte_lague_2021_federal_case() ->
+    Votes    = [ {spd, 11_949_756}, {cdu, 11_173_806}, {grn, 6_848_215}, {fdp, 5_316_698}, {afd, 4_802_097}, {lnk, 2_269_993} ],
+    Rankings = consensus:webster_sainte_lague_rankings(Votes, 735, 0.05),
+    %%% TODO 1 consituency seat won by independent Stefan Seidler.
+    %%% I think that's throwing off the calculations.
+    % Real SPD = 206
+    % Real CDU/CSU = 196
+    % Real Greens = 118
+    [ {spd, SPD, _}, {cdu, CDU, _}, {grn, GRN, _}, {fdp, 92, _}, {afd, 83, _}, {lnk, 39, _}] = Rankings,
+    ?assert(SPD > 205 andalso SPD < 209),
+    ?assert(CDU > 193 andalso CDU < 197),
+    ?assert(GRN > 117 andalso GRN < 120).
 
