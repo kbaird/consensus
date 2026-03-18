e41f813126008b15d48c4297550d8da9fdf19316
Kevin Baird <kevincbaird@gmail.com>
2026-03-18 16:46:37 -0700

    Fix some test names

--- test/droop_stv_test.erl
+++ test/droop_stv_test.erl
@@ -10,14 +10,14 @@ droop_stv_test_() ->
     {setup, fun droop_stv_setup/0,
             fun droop_stv_teardown/1,
             [
-                fun droop_two_winners_three_candidates_stv_stv_case/0,
-                %fun droop_five_winners_six_candidates_stv_stv_case/0,
-                fun droop_three_winners_five_candidates_stv_stv_case/0
+                fun droop_two_winners_three_candidates_stv_case/0,
+                %fun droop_five_winners_six_candidates_stv_case/0,
+                fun droop_three_winners_five_candidates_stv_case/0
             ]
     }.
 
 % https://en.wikipedia.org/wiki/Droop_quota#An_example_of_use_in_STV
-droop_two_winners_three_candidates_stv_stv_case() ->
+droop_two_winners_three_candidates_stv_case() ->
     BradOnlyBallots   = [ballot:make([brad])   || _ <- lists:seq(1, 30)],
     CarterOnlyBallots = [ballot:make([carter]) || _ <- lists:seq(1, 25)],
     MixedBallots      = [ballot:make([andrea, carter]) || _ <- lists:seq(1, 45)],
@@ -26,7 +26,7 @@ droop_two_winners_three_candidates_stv_stv_case() ->
     ?assertEqual([{andrea, 45}, {carter, 36}], Winners).
 
 % https://en.wikipedia.org/wiki/Comparison_of_the_Hare_and_Droop_quotas#Scenario_1
-droop_five_winners_six_candidates_stv_stv_case() ->
+droop_five_winners_six_candidates_stv_case() ->
     Ballot1s = [ballot:make([andrea, carter, brad]) || _ <- lists:seq(1, 31)],
     Ballot2s = [ballot:make([carter, andrea, brad]) || _ <- lists:seq(1, 30)],
     Ballot3s = [ballot:make([brad, andrea, carter]) || _ <- lists:seq(1, 2)],
@@ -42,7 +42,7 @@ droop_five_winners_six_candidates_stv_stv_case() ->
     {scott, _} = Scott.
 
 % https://en.wikipedia.org/wiki/Comparison_of_the_Hare_and_Droop_quotas#Scenario_2
-droop_three_winners_five_candidates_stv_stv_case() ->
+droop_three_winners_five_candidates_stv_case() ->
     Ballot1s = [ballot:make([alex, bobbie, chris]) || _ <- lists:seq(1, 50)],
     Ballot2s = [ballot:make([jo]) || _ <- lists:seq(1, 25)],
     Ballot3s = [ballot:make([kim]) || _ <- lists:seq(1, 24)],
